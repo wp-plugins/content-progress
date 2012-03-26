10eb49439297cb263441c5ff5f7f9b07e4263dcf
joedolson <joedolson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-26 16:15:57 +0000

    Update with new features, one bug fix. 1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/content-progress/trunk@523889 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- content-progress.php
+++ content-progress.php
@@ -3,7 +3,7 @@
 Plugin Name: Content Progress
 Plugin URI: http://www.joedolson.com/articles/content-progress/
 Description: Adds a column to each post/page or custom post type indicating whether content has been added to the page.
-Version: 1.1.1
+Version: 1.2.0
 Author: Joseph Dolson
 Author URI: http://www.joedolson.com/
 */
@@ -25,13 +25,14 @@ Author URI: http://www.joedolson.com/
 */
 // Prepend the new column to the columns array
 global $cp_version;
-$cp_version = '1.1.1';
+$cp_version = '1.2.0';
 
 load_plugin_textdomain( 'content-progress', false, dirname( plugin_basename( __FILE__ ) ) );
 
 
 function cp_column($cols) {
 	$cols['cp'] = __('Flag','content-progress');
+	$cols['cp_notes'] = __('Notes','content-progress');
 	return $cols;
 }
 
@@ -41,23 +42,28 @@ function cp_value($column_name, $id) {
 		$post = get_post($id);
 		$marked = get_post_meta($id,'_cp_incomplete',true);
 		$content = $post->post_content;
-		if ( $content == '' && !$marked ) {
-			echo "<img src='".plugins_url( 'images/empty.png', __FILE__ )."' alt='".__('Document is empty','content-progress')."' title='".__('Document is empty','content-progress')."' />";
-		} else if ( strlen($content) < 60 && !$marked ) {
-			echo "<img src='".plugins_url( 'images/partial.png', __FILE__ )."' alt='".__('Document has less than 60 characters of content.','content-progress')."' title='".__('Document has less than 60 characters of content.','content-progress')."' />";	
+		if ( $content == '' && $marked=='default' ) {
+			echo "<img src='".plugins_url( 'images/empty.png', __FILE__ )."' alt='".__('Document is empty','content-progress')."' class='$marked' title='".__('Document is empty','content-progress')."' />";
+		} else if ( strlen($content) < 60 && $marked=='default' ) {
+			echo "<img src='".plugins_url( 'images/partial.png', __FILE__ )."' alt='".__('Document has less than 60 characters of content.','content-progress')."' class='$marked' title='".__('Document has less than 60 characters of content.','content-progress')."' />";	
 		} else if ( $marked == 'true' ) {
-			echo "<img src='".plugins_url( 'images/incomplete.png', __FILE__ )."' alt='".__('Manually marked incomplete.','content-progress')."' title='".__('Manually marked incomplete.','content-progress')."' />";
+			echo "<img src='".plugins_url( 'images/incomplete.png', __FILE__ )."' alt='".__('Manually marked incomplete.','content-progress')."' class='$marked' title='".__('Manually marked incomplete.','content-progress')."' />";
 		} else if ( $marked == 'complete' ) {
-			echo "<img src='".plugins_url( 'images/complete.png', __FILE__ )."' alt='".__('Manually marked complete.','content-progress')."' title='".__('Manually marked complete.','content-progress')."' />";
+			echo "<img src='".plugins_url( 'images/complete.png', __FILE__ )."' alt='".__('Manually marked complete.','content-progress')."' class='$marked' title='".__('Manually marked complete.','content-progress')."' />";
 		} else if ( $marked == 'review' ) {
-			echo "<img src='".plugins_url( 'images/review.png', __FILE__ )."' alt='".__('Needs Editorial Review.','content-progress')."' title='".__('Needs Editorial Review.','content-progress')."' />";
+			echo "<img src='".plugins_url( 'images/review.png', __FILE__ )."' alt='".__('Needs Editorial Review.','content-progress')."' class='$marked' title='".__('Needs Editorial Review.','content-progress')."' />";
 		}		
+	}
+	if ($column_name == 'cp_notes') {
+		$notes = get_post_meta( $id, '_cp_notes',true );
+		echo "$notes";
 	}
 }
 
 function cp_return_value($value, $column_name, $id) {
-	if ($column_name == 'cp')
+	if ( $column_name == 'cp' || $column_name == 'cp_notes' ) {
 		$value = $id;
+	}
 	return $value;
 }
 
@@ -65,25 +71,31 @@ function cp_return_value($value, $column_name, $id) {
 function cp_css() {
 ?>
 <style type="text/css">
-	#cp { width: 50px; } 
+#cp { width: 40px; } 
+#cp_notes { width: 120px; }
+.inline-edit-col-left legend { text-transform:uppercase; font-weight: 700; }	
 </style>
 <?php	
 }
 
 // Actions/Filters for various tables and the css output
 function cp_add() {
+	$settings = get_option( 'cp_settings' );
 	add_action('admin_head', 'cp_css');
-
-	add_filter('manage_posts_columns', 'cp_column');
-	add_action('manage_posts_custom_column', 'cp_value', 10, 2);
-
-	add_filter('manage_pages_columns', 'cp_column');
-	add_action('manage_pages_custom_column', 'cp_value', 10, 2);
-
-
+	
+	if ( in_array( 'post', $settings ) ) {
+		add_filter('manage_posts_columns', 'cp_column');
+		add_action('manage_posts_custom_column', 'cp_value', 10, 2);
+	}
+	if ( in_array( 'page', $settings ) ) {
+		add_filter('manage_pages_columns', 'cp_column');
+		add_action('manage_pages_custom_column', 'cp_value', 10, 2);
+	}
 	foreach ( get_post_types() as $types ) {
-		add_action("manage_${types}_columns", 'cp_column');			
-		add_filter("manage_${types}_custom_column", 'cp_return_value', 10, 3);
+		if ( in_array( $types, $settings ) ) {
+			add_action("manage_${types}_columns", 'cp_column');			
+			add_filter("manage_${types}_custom_column", 'cp_return_value', 10, 3);
+		}
 	}
 
 }
@@ -159,13 +171,55 @@ function list_review($atts) {
 }
 add_shortcode('needs_review','list_review');
 
+add_action('quick_edit_custom_box', 'cp_quickedit_show', 10, 2);
+ 
+function cp_quickedit_show( $col, $type ) {
+	$settings = get_option( 'cp_settings' );
+	if ( in_array( $type, $settings ) ) {
+		if ( $col == 'cp' ) {
+			$label = 'Flag';
+			$name = '_cp_incomplete';
+			$field = "<select name='$name' id='$name'>
+						<option value='review'>Needs Review</option>
+						<option value='true'>Mark as incomplete</option>
+						<option value='complete'>Mark as complete</option>
+						<option value='default'>Default</option>
+					</select>";
+			$fieldset = "<fieldset class=\"inline-edit-col-left inline-edit-$type\"><legend>".__('Content Progress','content-progress')."</legend>";
+			$close_fieldset = "";		
+		} else if ( $col == 'cp_notes' ) {
+			$label = 'Notes';
+			$name = '_cp_notes';
+			$field = "<textarea rows='1' cols='22' name='$name' id='$name'></textarea>";
+			$fieldset = '';
+			$close_fieldset = "</fieldset>";
+		}
+	}
+?>
+<?php echo $fieldset; ?>
+<div class="inline-edit-col inline-edit-<?php echo $col; ?>">
+<div class="inline-edit-group"><label for="<?php echo $name; ?>"><span class="title"><?php echo $label; ?></span></label>
+ <?php echo $field; ?>
+</div>
+</div><?php echo $close_fieldset; ?>
+<?php 
+}
+
+add_action('admin_footer-edit.php', 'cp_admin_edit_foot', 11 );
+
+/* load scripts in the footer */
+function cp_admin_edit_foot() {
+    echo '<script type="text/javascript" src="', plugins_url('scripts/admin_edit.js', __FILE__), '"></script>';
+}
 
 function cp_post_meta( $id ) {
-	if ( isset($_POST['_inline_edit']) ) { return; }
-
 	if ( isset( $_POST['_cp_incomplete'] ) ) {
 		$incomplete = $_POST[ '_cp_incomplete' ];
 		update_post_meta( $id, '_cp_incomplete', $incomplete );			
+	}
+	if ( isset( $_POST['_cp_notes'] ) ) {
+		$notes = $_POST[ '_cp_notes' ];
+		update_post_meta( $id, '_cp_notes', $notes );
 	}
 }
 
@@ -195,6 +249,7 @@ function cp_add_outer_box() {
 function cp_add_inner_box() {
 	global $post_id;
 	$cp = get_post_meta($post_id, '_cp_incomplete',true );
+	$notes = get_post_meta($post_id, '_cp_notes',true );
 	if ( $cp == 'true' ) { $tchecked = ' checked="checked"'; } else { $tchecked = ''; }
 	if ( $cp == 'complete' ) { $cchecked = ' checked="checked"'; } else { $cchecked = ''; }
 	if ( $cp == 'review' ) { $rchecked = ' checked="checked"'; } else { $rchecked = ''; }	
@@ -205,7 +260,7 @@ function cp_add_inner_box() {
 	echo "<li><input type='radio' name='_cp_incomplete' value='review' id='_cp_incomplete'$rchecked /> <label for='_cp_incomplete'>".__('Mark as "Needs Review"','content-progress')."</label></li>";	
 	echo "<li><input type='radio' name='_cp_incomplete' value='default' id='_cp_incomplete'$dchecked /> <label for='_cp_incomplete'>".__('Default','content-progress')."</label></li>";
 	echo "</ul>";
-	echo $cp;
+	echo "<p><label for='_cp_notes'>".__('Notes:','content-progress')."</label><br /><textarea name='_cp_notes' id='_cp_notes'>$notes</textarea></p>";
 }
 add_action( 'admin_menu','cp_add_outer_box' );
 
@@ -268,7 +323,7 @@ Version: $theme_version
 ==Active Plugins:==
 $plugins_string
 ";
-	if ( isset($_POST['mc_support']) ) {
+	if ( isset($_POST['cp_support']) ) {
 		$nonce=$_REQUEST['_wpnonce'];
 		if (! wp_verify_nonce($nonce,'content-progress-nonce') ) die("Security check failed");	
 		$request = stripslashes($_POST['support_request']);
@@ -308,25 +363,64 @@ $plugins_string
 		<label for='support_request'>Support Request:</label><br /><textarea name='support_request' id='support_request' cols='80' rows='10'>".stripslashes($request)."</textarea>
 		</p>
 		<p>
-		<input type='submit' value='".__('Send Support Request','content-progress')."' name='mc_support' class='button-primary' />
+		<input type='submit' value='".__('Send Support Request','content-progress')."' name='cp_support' class='button-primary' />
 		</p>
 		<p>".
 		__('The following additional information will be sent with your support request:','content-progress')
 		."</p>
-		<div class='mc_support'>
+		<div class='cp_support'>
 		".wpautop($data)."
 		</div>
 		</div>
 	</form>";
 }
+
+
+function cp_settings() {
+	if ( isset($_POST['cp_settings']) ) {
+		$nonce=$_REQUEST['_wpnonce'];
+		if (! wp_verify_nonce($nonce,'content-progress-nonce') ) die("Security check failed");	
+		$settings = $_POST['cp_post_types'];
+		update_option( 'cp_settings', $settings );
+	}
+	$settings = get_option( 'cp_settings' );
+	$post_types = get_post_types( array('public'=>true), 'objects' );
+	$cp_post_types = $settings;
+	$my_post_types = '';
+		foreach( $post_types as $type ) {
+			if ( in_array(  $type->name , $cp_post_types ) || empty($cp_post_types) ) { $selected = ' selected="selected"'; } else { $selected = ''; }
+			$my_post_types .= "<option value='$type->name'$selected>$type->name</option>";
+		}				
+
+	
+	echo "
+	<form method='post' action='".admin_url('options-general.php?page=content-progress/content-progress.php')."'>
+		<div><input type='hidden' name='_wpnonce' value='".wp_create_nonce('content-progress-nonce')."' /></div>
+		<div>
+		<p>
+		<label for='post_types'>".__('Enabled for these post types:','content-progress')."</label><br />
+		<select multiple='multiple' name='cp_post_types[]' id='post_types'>
+		$my_post_types;
+		</select>
+		</p>
+		<p>
+		<input type='submit' value='".__('Update Settings','content-progress')."' name='cp_settings' class='button-primary' />
+		</p>
+		</div>
+	</form>";
+}
+
 function cp_support_page() {
 ?>
 <div class="wrap">
-<div class="cp-support" id="poststuff">
-<div class="postbox" id="get-support">
-<h3><?php _e('Get Plug-in Support','content-progress'); ?></h3>
+<div class="cp-settings" id="poststuff">
+<div class="postbox" id="settings">
+<h3><?php _e('Content Progress Settings','content-progress'); ?></h3>
 <div class="inside">
 <?php cp_show_support_box(); ?>
+
+<?php cp_settings(); ?>
+
 <h4><?php _e('Icon guide:','content-progress'); ?></h4>
 <ul class="icon-guide">
 <?php 
@@ -337,6 +431,15 @@ function cp_support_page() {
 	<li><img src='".plugins_url( 'images/review.png', __FILE__ )."' alt='".__('Needs Editorial Review.','content-progress')."' />".__('Needs Editorial Review.','content-progress')."</li>";
 ?>
 </ul>
+</div>
+</div>
+</div>
+<div class="cp-support" id="poststuff">
+<div class="postbox" id="get-support">
+<h3><?php _e('Get Plug-in Support','content-progress'); ?></h3>
+<div class="inside">
+<?php cp_show_support_box(); ?>
+
 <?php cp_get_support_form(); ?>
 </div>
 </div>
--- readme.txt
+++ readme.txt
@@ -1,18 +1,20 @@
 === Content Progress ===
 Contributors: joedolson
 Donate link: http://www.joedolson.com/donate.php
-Tags: page, post, admin, developer, tools, progress, content
+Tags: page, post, admin, developer, tools, progress, content, editorial, review, overview
 Requires at least: 3.2.1
 Tested up to: 3.3.1
 Stable tag: trunk
 
-Adds a column to each post/page or custom post type indicating whether content has been added to the page.
+Add an icon & your notes to each Post, Page, or custom post type indicating the current status of the post.
 
 == Description ==
 
 Content Progress adds a set of icons into your content listings (Posts, Pages, and custom post types) indicating whether the document is complete. While the plug-in has no way to know whether the page has been completely laid out, etc., it can identify whether there is actually content on the page. If there isn't, that document will be flagged as empty. If there's only a small quantity of content on the page, that document will be flagged as partial. 
 
 You also have the option to flag any document as incomplete, regardless of the content in that page, in which case it will have a separate 'incomplete' flag. 
+
+To help communication between groups (or remembering where you are for large sites!), you can also add notes to each post.
 
 This simply provides a quick and efficient way to scan over which documents in your web site are finished, since it is frequently the case in my own development that I add all new pages (for the purpose of building menus and navigation) before actually filling those pages with content.
 
@@ -21,6 +23,13 @@ There are also four shortcodes available for generating front-facing lists of pa
 Translations are always welcome! The translation file is in the download. Granted, there isn't a lot of text in this plug-in.
 
 == Changelog ==
+
+= 1.2.0 =
+
+* Added 'notes' column to posts tables.
+* Added selection options for progress label and notes fields to Quick Edit mode.
+* Added option to limit use of plug-in to specific content types.
+* Bug fix: default settings were not rendered in posts tables.
 
 = 1.1.1 =
 
